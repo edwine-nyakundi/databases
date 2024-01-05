b9f064fc4663ab879619004412aba6443236a0f1
edwine-nyakundi <145298218+edwine-nyakundi@users.noreply.github.com>
2024-01-05 22:27:51 +0300

    database&table creation using sql

---
+++ sql_database_table.sql
@@ -0,0 +1,7 @@
+CREATE  DATABASE 'ariel_technologies';
+use 'ariel_technologies';
+CREATE TABLE 'employee'(
+    id int NOT NULL,
+    first_name varchar(20) NOT NULL,
+    LAST_NAME varchar(20) NOT null
+);
